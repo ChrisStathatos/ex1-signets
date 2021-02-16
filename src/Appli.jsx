b4ff49406027d049db9d1f1b334e1e5eb456d34d
Christopher Stathatos <christopherstathatos@hotmail.com>
2021-02-15 22:42:33 -0500

    avancement

---
+++ src/Appli.jsx
@@ -0,0 +1,12 @@
+import './Appli.scss';
+import Entete from './Entete.jsx';
+
+function Appli() {
+  return(
+  <div className="Appli">
+  <Entete/>
+</div>
+  );
+}
+
+export default Appli;
---
+++ src/Entete.jsx
@@ -0,0 +1,18 @@
+import './Entete.scss';
+
+function Entete(){
+
+    return(
+        <header className="Entete">
+        <div>Signets</div>
+        <ul className="navUtilisateur">
+          <li>Christopher Stathatos</li>
+          <span className="photo"></span>
+        </ul>
+      </header>
+    )
+}
+
+
+
+export default Entete;
